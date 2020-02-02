4346b2e34315449a21adfad0dd11bcf78f72ee67
Kwane Smith <smithkwane6@gmail.com>
2020-02-02 19:09:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
     smallest_hash_key ||= key
     smallest_hash_key = key
       if val < name_hash[smallest_hash_key]
+      end
+    smallest_hash_key
   end
-  smallest_hash_key
 end
