e1878fbc98d76da2cee828f6b8dd8082bbe8ac5f
Kwane Smith <smithkwane6@gmail.com>
2020-02-02 19:15:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_key = nil
 min_value = nil
 name_hash.each do |key, value|
-  min_key = key if min_key == nil 
+  min_key = key if min_key == nil
   min_value = value if min_value == nil
   if value < min_value
     min_value = value
@@ -13,5 +13,5 @@ name_hash.each do |key, value|
   end
 end
   min_key
-  end 	
+  end
 end
